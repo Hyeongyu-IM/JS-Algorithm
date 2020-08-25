b9634c455dafdc1f60032178cc6eccdd5cdf183a
Hyeongyu-IM <64323969+Hyeongyu-IM@users.noreply.github.com>
2020-08-25 23:18:40 +0900

    complete

---
+++ 홀수사이 - 넣기.js
@@ -0,0 +1,13 @@
+function DashInsert(string) {
+    let result = ''
+    for (let i = 0; string.length > i; i++) {
+        if (string[i] % 2 === 1 && string[i + 1] % 2 === 1) {
+            result = result + string[i] + '-'
+        } else {
+            result = result + string[i]
+        }
+    }
+    return result
+}
+
+//간단한 문제였지만 저기에 스트링 i를 붙이면 된다는것을 인식하기 어려웠다 답지볼까 생각했지만 안봄 ㅎㅎ
